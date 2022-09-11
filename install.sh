379017db3d6408a3ebb7d79e634cf919b2a04052
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 20:52:40 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg update 
+pkg upgrade
+pkg install wget
+apt update 
+apt upgrade
+apt install wget
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+rm ngrok-v3-stable-linux-amd64.tgz
+chmod +x *
+clear
+echo "Ngrok instalado com sucesso :)"
