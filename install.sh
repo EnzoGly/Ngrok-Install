e4f341f6e7f1e6579634b3ed0d808290d5c23b16
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 22:19:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-pkg update 
-pkg upgrade
-pkg install python3
-pkg install wget
 apt update 
 apt upgrade
 apt install python3
@@ -15,8 +11,8 @@ wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 rm ngrok-v3-stable-linux-amd64.tgz install.sh
 chmod +x *
+cp programa.py ngrok executar.sh ..
 cd
-chmod +x *
 clear
 echo "Ngrok instalado com sucesso !!!"
 echo "Configure seu token ngrok :)"
