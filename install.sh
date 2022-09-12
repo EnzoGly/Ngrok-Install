29f1a7d998c548ec7e207dbc94fb04c5e1183f15
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 21:02:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 pkg update 
 pkg upgrade
+pkg install python3
 pkg install wget
 apt update 
 apt upgrade
+apt install python3
 apt install wget
+git clone https://github.com/EnzoGly/API
+cd API 
+cp programa.py ..
+rm -rf API
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 cp ngrok ..
