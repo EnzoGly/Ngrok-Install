e7e604794d574c02aa04887d8db49a9e6c4ce782
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 21:08:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,13 @@ apt install wget
 git clone https://github.com/EnzoGly/API
 cd API 
 cp programa.py ..
+cd ..
 rm -rf API
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
-cp ngrok ..
+cp ngrok executar.sh ..
 rm ngrok-v3-stable-linux-amd64.tgz
+chmod +x *
 cd
 chmod +x *
 clear
